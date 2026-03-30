33fa77a1dd43bf3e693ec81740ac53f50b673db3
Amir Pournasserian <amir.pournasserian@live.com>
2026-03-30 12:30:29 -0400

    Make plugin exceptions serializable
    Mark plugin exception types as [Serializable] and add parameterless and protected deserialization constructors (SerializationInfo, StreamingContext) to satisfy CA2229 and enable proper serialization. Adds System.Runtime.Serialization using and applies to PluginDiscoveryException, PluginInitializerException, and PluginLoaderException.

--- src/Plugins/FluentCMS.Infrastructure.Plugins/Discovery/PluginDiscoveryException.cs
+++ src/Plugins/FluentCMS.Infrastructure.Plugins/Discovery/PluginDiscoveryException.cs
@@ -1,10 +1,20 @@
+using System.Runtime.Serialization;
+
 namespace FluentCMS.Infrastructure.Plugins.Discovery;
 
 /// <summary>
 /// Exception thrown when plugin discovery fails.
 /// </summary>
+[Serializable]
 public class PluginDiscoveryException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the PluginDiscoveryException class.
+    /// </summary>
+    public PluginDiscoveryException()
+    {
+    }
+
     /// <summary>
     /// Initializes a new instance of the PluginDiscoveryException class.
     /// </summary>
@@ -19,6 +29,15 @@ public class PluginDiscoveryException : Exception
     /// <param name="message">The error message.</param>
     /// <param name="innerException">The inner exception.</param>
     public PluginDiscoveryException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the PluginDiscoveryException class with serialized data (CA2229).
+    /// </summary>
+    /// <param name="info">The SerializationInfo that holds the serialized object data.</param>
+    /// <param name="context">The StreamingContext that contains contextual information about the source or destination.</param>
+    protected PluginDiscoveryException(SerializationInfo info, StreamingContext context) : base(info, context)
     {
     }
 }
