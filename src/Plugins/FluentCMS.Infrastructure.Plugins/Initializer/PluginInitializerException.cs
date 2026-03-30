91fd1bd2c50e4d0c8ff359fb0f5796612bb835ea
Amir Pournasserian <amir.pournasserian@live.com>
2026-03-30 11:52:36 -0400

    fix: add XML documentation comments to PluginInitializerException (#22)
    Add missing XML documentation comments (<summary>, <param> tags) to
    PluginInitializerException, consistent with PluginDiscoveryException
    and PluginLoaderException in the same library.

--- src/Plugins/FluentCMS.Infrastructure.Plugins/Initializer/PluginInitializerException.cs
+++ src/Plugins/FluentCMS.Infrastructure.Plugins/Initializer/PluginInitializerException.cs
@@ -1,11 +1,23 @@
-﻿namespace FluentCMS.Infrastructure.Plugins.Initializer;
+namespace FluentCMS.Infrastructure.Plugins.Initializer;
 
+/// <summary>
+/// Exception thrown when plugin initialization fails.
+/// </summary>
 public class PluginInitializerException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the PluginInitializerException class.
+    /// </summary>
+    /// <param name="message">The error message.</param>
     public PluginInitializerException(string message) : base(message)
     {
     }
 
+    /// <summary>
+    /// Initializes a new instance of the PluginInitializerException class.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="innerException">The inner exception.</param>
     public PluginInitializerException(string message, Exception innerException) : base(message, innerException)
     {
     }
